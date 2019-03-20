5a9cf7cc6b9b5631667a1faf40f6650ada04f83a
Saverzus <47740639+Saverzus@users.noreply.github.com>
2019-03-20 09:30:01 +0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,113 @@
+'use sctrict';/*
+var modalWindowIdContainer = document.getElementById('modalWindowIdContainer');
+var modalWindowId_close = document.getElementById('modalWindowId_close');
+
+var modalWindowId_img = document.getElementById('modalWindowId_img');
+var main__6_block_id = document.getElementById('main__6_block_id');
+var modalWindowId_num = document.getElementById('modalWindowId_num');
+
+
+var pValue = document.getElementById('pValue');
+
+
+
+var elem = document.getElementById('test');
+
+elem.onclick = func;
+function func(){
+	modalWindowIdContainer.style.display = 'inline-block';
+
+}
+
+function closeModal() {
+	modalWindowIdContainer.style.display = 'none';
+}
+
+window.onclick = function(event) {
+	if (event.target == modalWindowIdContainer) {
+		modalWindowIdContainer.style.display = 'none';
+	}
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function openModal() {
+	modalWindowIdContainer.style.display = 'inline-block';
+}
+function closeModal() {
+	modalWindowIdContainer.style.display = 'none';
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function myFunction() {
+  var x = document.createElement("IMG");
+  x.setAttribute("src", "img_pulpit.jpg");
+  x.setAttribute("width", "304");
+  x.setAttribute("height", "228");
+  x.setAttribute("alt", "The Pulpit Rock");
+  document.body.appendChild(x);
+}
+*/
